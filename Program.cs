053d354b08db340c14301b144e496d2de81a502e
Michael Wang <michael.wang@myob.com>
2020-08-24 12:01:27 +1000

    removed Console.WriteLine statements used for debugging

--- Program.cs
+++ Program.cs
@@ -10,54 +10,43 @@ namespace Payslip
 
             Console.Write("Please input your name: ");
             string name = Console.ReadLine();
-            Console.WriteLine($"DEBUG: name is {name}");
 
             Console.Write("Please input your surname: ");
             string surname = Console.ReadLine();
-            Console.WriteLine($"DEBUG: surname is {surname}");
             
             Console.Write("Please enter your annual salary: ");
             int annualSalary = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"DEBUG: annualSalary is {annualSalary}");
 
             Console.Write("Please enter your super rate: ");
             double superRate = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine($"DEBUG: superRate is {superRate}");
 
             Console.Write("Please enter your payment start date: ");
             string startDate = Console.ReadLine();
-            Console.WriteLine($"DEBUG: startDate is {startDate}");
 
             Console.Write("Please enter your payment end date: ");
             string endDate = Console.ReadLine();
-            Console.WriteLine($"DEBUG: endDate is {endDate}");
 
             double grossIncome = annualSalary / 12;
             double incomeTax;
             if (annualSalary >= 180001)
             {
                 incomeTax = (54232 + (annualSalary - 180000) * 0.45) / 12;
-                Console.WriteLine("Hit tier 1");
             }
             else if (annualSalary >= 87001)
             {
                 incomeTax = (19822 + (annualSalary - 87000) * 0.37) / 12;
-                Console.WriteLine("Hit tier 2");
             }
             else if (annualSalary >= 37001)
             {
                 incomeTax = (3572 + (annualSalary - 37000) * 0.325) / 12;
-                Console.WriteLine("Hit tier 3");
             }
             else if (annualSalary >= 18201)
             {
                 incomeTax = ((annualSalary - 18200) * 0.19) / 12;
-                Console.WriteLine("Hit tier 4");
             }
             else
             {
                 incomeTax = 0;
-                Console.WriteLine("Hit tier 5");
             }
 
             double netIncome = grossIncome - incomeTax;
